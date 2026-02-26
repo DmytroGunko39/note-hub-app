90731442953e128973217854c1217dd7dbb3b396
Dmytro_Gunko <dimagunko99@gmail.com>
2026-02-26 15:46:14 +0100

    update AuthProvider to store accessToken from session

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { checkSession, getMe } from '@/lib/api/clientApi';
+import { refreshSession, getMe } from '@/lib/api/clientApi';
 import { useAuthStore } from '@/lib/store/authStore';
 import { useEffect } from 'react';
 
@@ -9,20 +9,37 @@ type Props = {
 };
 
 const AuthProvider = ({ children }: Props) => {
-  const setAuth = useAuthStore((set) => set.setUser);
-  const clearAuth = useAuthStore((set) => set.clearIsAuthenticated);
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+  const setUser = useAuthStore((state) => state.setUser);
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+
   useEffect(() => {
-    const fetchSession = async () => {
-      const res = await checkSession();
-      if (res) {
-        const user = await getMe();
-        setAuth(user);
-      } else {
+    const initAuth = async () => {
+      try {
+        // Get accessToken from session endpoint
+        const sessionData = await refreshSession();
+
+        if (sessionData?.accessToken) {
+          // Store accessToken in Zustand (will be used by Axios interceptor)
+          setAccessToken(sessionData.accessToken);
+
+          // Fetch user data with the new token
+          const user = await getMe();
+          if (user) {
+            setUser(user);
+          }
+        } else {
+          clearAuth();
+        }
+      } catch (error) {
+        console.error('Auth initialization failed:', error);
         clearAuth();
       }
     };
-    fetchSession();
-  }, [clearAuth, setAuth]);
+
+    initAuth();
+  }, [setAccessToken, setUser, clearAuth]);
+
   return children;
 };
 
